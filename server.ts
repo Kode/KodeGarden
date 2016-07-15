95935d3f986b1ac8bc38250e2a5fe984626308ef
Robert Konrad <git2016@robdangero.us>
2016-07-15 18:00:21 +0200

    Configured project for vscode.

---
+++ server.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+
+let app = express();
+let expressWs = require('express-ws')(app);
+
+let wsapp: any = app;
+
+wsapp.ws('/', (connection, request) => {
+	connection.on('message', message => {
+		let messagedata = JSON.parse(message);
+		//connection.send(JSON.stringify({method: 'newuser', data: {user: userdata}}));
+	});
+});
+
+app.use('/', express.static('./Client'));
+
+app.listen(8765);
+
+console.log('The monkeys are listening...');
