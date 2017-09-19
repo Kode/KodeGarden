292f9848b25e39bc4eb473c1e147c24365bb3613
Robert Konrad <git2016@robdangero.us>
2017-09-19 17:48:26 +0200

    Add a setup script and a readme

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Kode Garden
+
+Official installation is located at http://kodegarden.org
+
+To run locally:
+
+- Install Git (probably already done)
+- Install Node.js (version 8.x or higher)
+- Install TypeScript (npm install -g typescript)
+- Run setup.js (node setup)
+- cd Server
+- node out/kodegarden
+- Open localhost:9090 in your browser
+
+The project is setup for development in Visual Studio Code.
+Just load the folder, hit Ctrl+Shift+b to continually build the sources and hit F5 to run the server in the debugger.
---
+++ setup.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+
+child_process.spawnSync('npm', ['install'], {stdio: 'inherit', shell: true});
+child_process.spawnSync('npm', ['install'], {cwd: 'Client', stdio: 'inherit', shell: true});
+child_process.spawnSync('npm', ['install'], {cwd: 'Server', stdio: 'inherit', shell: true});
+
+child_process.spawnSync('tsc', [], {cwd: 'Client', stdio: 'inherit', shell: true});
+child_process.spawnSync('tsc', [], {cwd: 'Server', stdio: 'inherit', shell: true});
+
+fs.mkdirSync('Projects');
+fs.mkdirSync(path.join('Projects', 'Archives'));
+fs.mkdirSync(path.join('Projects', 'Checkouts'));
+fs.mkdirSync(path.join('Projects', 'Clones'));
+fs.mkdirSync(path.join('Projects', 'Temp'));
+
+child_process.spawnSync('git', ['clone', '--bare', 'https://github.com/Kode/KodeGarden-InitialRepository.git', 'Repository'], {cwd: 'Projects', stdio: 'inherit', shell: true});
