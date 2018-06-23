6ce5682e62b0c19a0c9d0c1ef13cbbc7c57cb28b
Robert Konrad <git2016@robdangero.us>
2018-06-23 09:39:03 +0200

    Remove old TypeScript client

--- Client/init.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-	baseUrl: './out',
-	paths: {
-		domReady: '../node_modules/domready/ready.min',
-		vs: '../node_modules/monaco-editor/min/vs'
-	}
-});
-
-requirejs(['Editor'], () => {});
--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,7 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 
-const clientProject = ts.createProject('Client/tsconfig.json');
 const serverProject = ts.createProject('Server/tsconfig.json');
-
-gulp.task("compileClient", () => {
-	return clientProject.src()
-		.pipe(sourcemaps.init())
-		.pipe(clientProject()).js
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('Client/out'));
-});
 
 gulp.task("compileServer", () => {
 	return serverProject.src()
@@ -21,7 +12,6 @@ gulp.task("compileServer", () => {
 		.pipe(gulp.dest('Server/out'));
 });
 
-gulp.task('watch', ['compileClient', 'compileServer'], () => {
-	gulp.watch(['Client/**/*.ts', 'Client/**/*.tsx', 'Shared/**/*.ts'], ['compileClient']);
+gulp.task('watch', ['compileServer'], () => {
 	gulp.watch(['Server/**/*.ts', 'Shared/**/*.ts'], ['compileServer']);
 });
