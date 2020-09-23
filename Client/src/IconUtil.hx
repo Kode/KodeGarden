7a7ee07fe0bdd4093361a9dc4043f184e7c33698
Ian Harrigan <ianharrigan@hotmail.com>
2020-09-23 15:31:46 +0200

    add more icons

--- Client/src/IconUtil.hx
+++ Client/src/IconUtil.hx
@@ -1,15 +1,6 @@
 package;
 
 class IconUtil {
-    public static function assetIcon(asset:String):String {
-        var icon = "img/picture_grey.png";
-        if (StringTools.endsWith(asset, ".wav") || StringTools.endsWith(asset, ".ogg") || StringTools.endsWith(asset, ".mp3")) {
-            icon = "img/audio-file_grey.png";
-        }
-
-        return icon;
-    }
-    
     public static function iconFromExtension(name:String):String {
         var icon = "icons/file.png";
         if (StringTools.endsWith(name, ".hx")) {
@@ -18,6 +9,10 @@ class IconUtil {
             icon = "icons/puzzle.png";
         } else if (StringTools.endsWith(name, ".png") || StringTools.endsWith(name, ".jpg") || StringTools.endsWith(name, ".jpeg") || StringTools.endsWith(name, ".bmp")) {
             icon = "icons/picture.png";
+        } else if (StringTools.endsWith(name, ".wav") || StringTools.endsWith(name, ".mp3") || StringTools.endsWith(name, ".flac")) {
+            icon = "icons/sound.png";
+        } else if (StringTools.endsWith(name, ".map")) {
+            icon = "icons/map.png";
         }
         return icon;
     }
